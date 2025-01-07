287b7ef4d2464602842ee2c09a7a081bfec57dc3
abegorov <abegorov@github.com>
2025-01-07 09:26:42 +0300

    Поддержка tls при подключении к БД

--- roles/glpi/templates/config_db_slave.php
+++ roles/glpi/templates/config_db_slave.php
@@ -5,6 +5,10 @@ class DBSlave extends DBmysql {
    public $dbuser = '{{ glpi_db_user }}';
    public $dbpassword = '{{ glpi_db_password | urlencode }}';
    public $dbdefault = '{{ glpi_db_name }}';
+   public $dbssl = true;
+   public $dbsslca = '{{ glpi_db_cafile }}';
+   public $dbsslcert = '{{ glpi_db_certfile }}';
+   public $dbsslkey = '{{ glpi_db_keyfile }}';
    public $use_utf8mb4 = true;
    public $allow_myisam = false;
    public $allow_datetime = false;
